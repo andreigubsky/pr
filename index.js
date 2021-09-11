4258631d0bedc0698e76dce0e81ca0c40676155b
User <andrei.gubsky@gmail.com>
2021-09-11 15:28:44 +0300

    '4-th'

---
+++ index.js
@@ -0,0 +1,16 @@
+var styleUl = document.querySelector('.styleUl');
+var src = document.getElementsByClassName('styleUl');
+var li = document.createElement('li');
+
+function addLi(){
+    var liLast = document.createElement('li');
+    liLast.innerHTML = '';
+    //alert(src);
+    ul.append(liLast); 
+  }
+  
+
+  function addStyleOut(){
+    //document.head.appendChild(document.createElement('style'));
+    alert ("Стили внешние залетели");
+  } 
---
+++ vars.js
@@ -0,0 +1,24 @@
+
+
+
+
+let lowCourse;
+let highCourse;
+
+let date;
+let interval;
+
+let marketDepthSale;
+let marketDepthSale;
+
+let buyCourse;
+let sellCourse;
+let curCourse;
+
+let amount;
+let ROI;
+let marginSale;
+
+let order;
+let dataUpdeteInterval;
+
