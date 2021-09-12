8297dcb4e52b4b952bb980e13a8fb4f4680a388c
Andrei Gubsky <andrei.gubsky@gmail.com>
2021-09-12 19:16:05 +0300

    chart

--- math.js
+++ math.js
@@ -8,24 +8,40 @@ var strategyLong;
 //b - число пересечения с осью
 //a - значение прироста следующего значения
 
-var a;
-var x;
-var b;
+var a = 1;
+var x = 2;
+var b = 3;
 
 function trendLineS(){
-    var f = ax;
+    var f = a*x;
+    alert(f);
 };
+
+
+//Линейная y = 1*2+3=5
 
 function trendLineLine(){
-    var y = ax+b;
+    var y = a*x+b;
+    alert (y);
 };
 
+//Экспоненциальная z= 3+4+16=23
+
+var c1 = 1;
+var c2 = 2;
 function trendLinePoly(){
     var z = b + c1*Math.pow(x, 2) + c2*Math.pow(x, 3);
     alert(z);
 };
 
+//Логарифмическая l= 2*0,693+3=4,38
 
+var c = 2;
+var d = 3;
+function trendLineLog(){
+    var l = c * Math.log(x) +d;
+    alert(l);
+};
 
 
 
